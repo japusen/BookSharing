f0ca020e0920b796119e828444bfeb3aefea5963
japusen <japusen32@gmail.com>
2014-09-20 17:29:59 -0700

    clean up

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^', include('login.urls')),
+    url(r'^home/', include('content.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
