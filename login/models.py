8407cc39d1f1f6c85f6dc9a350c45f73d513547c
japusen <japusen32@gmail.com>
2014-09-20 22:34:16 -0700

    content models created. started navbar.

--- login/models.py
+++ login/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
 class UserInfo(models.Model):
 	umail = models.EmailField(primary_key=True)
 	password = models.CharField(max_length=15)
